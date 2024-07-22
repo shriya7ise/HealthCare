fffeb51114d1fb061d54c47a6992d05a470e742d
Shriya <iseshriya@gmail.com>
2024-07-22 09:39:07 +0530

    Delete library_initialize.py

--- library_initialize.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
-
-def load_data(file_path):
-    return pd.read_csv(file_path)
